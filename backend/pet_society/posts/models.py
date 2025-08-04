58769c74d9114c5b2a6eaa6ce9c5e13e5480498a
Ahmed Hossam <107175822+AhmedHossamAshour@users.noreply.github.com>
2025-08-04 23:34:05 +0300

    add posts models

--- backend/pet_society/posts/models.py
+++ backend/pet_society/posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
